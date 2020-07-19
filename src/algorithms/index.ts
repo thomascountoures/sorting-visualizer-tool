279882643438c1807953a4a6c872f04db7d5d576
Thomas Countoures <rainwave@gmail.com>
2020-07-19 14:02:30 -0700

    fix bug where merge sort would freeze browser sometimes, add bubble sort algorithm

--- src/algorithms/index.ts
+++ src/algorithms/index.ts
@@ -9,7 +9,7 @@ export const mergeSort = (array: number[]) => {
   while (i < leftHalfSorted.length && j < rightHalfSorted.length) {
     if (leftHalfSorted[i] < rightHalfSorted[j]) {
       finalSortedResult.push(leftHalfSorted[i++]);
-    } else if (rightHalfSorted[j] < leftHalfSorted[i]) {
+    } else {
       finalSortedResult.push(rightHalfSorted[j++]);
     }
   }
@@ -21,4 +21,53 @@ export const mergeSort = (array: number[]) => {
   }
 
   return finalSortedResult;
+};
+
+export const bubbleSort = (array: number[]) => {
+  let currentIndex = 0;
+  let sortingOccurring = true;
+  let sorted = false;
+
+  while (sortingOccurring) {
+    let currValue = array[currentIndex];
+    let nextValue = array[currentIndex + 1];
+
+    console.log(`current value is ${currValue}`);
+    console.log(`next value is ${nextValue}`);
+
+    // console.log('--------------------------------------');
+
+    if (currValue > nextValue) {
+      console.log('swapping values...');
+      // swap values
+      array.splice(currentIndex, 1, nextValue);
+      array.splice(currentIndex + 1, 1, currValue);
+
+      //console.log(`what is sorted? ${sorted}`);
+      // don't keep setting sorted to true if it's already true
+      if (!sorted) {
+        sorted = true;
+        console.log('sorted becomes true');
+      }
+    }
+
+    // if this is the last iteration in this run
+    if (currentIndex + 1 === array.length - 1) {
+      console.log(
+        `last iteration in this loop... sorted was ${sorted} -------------`
+      );
+      currentIndex = 0;
+      if (!sorted) {
+        sortingOccurring = false;
+        console.log('done');
+        return array;
+      } else {
+        // if a sort did occur during this pass, reset for the next run
+        sorted = false;
+      }
+    } else {
+      // not the last iteration in this run, keep going
+      currentIndex += 1;
+    }
+  }
 };
